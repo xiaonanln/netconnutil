2dd6717f9df210c0edf081887299981237607e10
Simon Lin <simonlin@google.com>
2020-02-07 23:09:56 +0800

    BufferedConn works

---
+++ BufferedConn.go
@@ -0,0 +1,50 @@
+package netconnutil
+
+import (
+	"bufio"
+	"net"
+)
+
+// NewBufferedConn creates a new connection with buffered write based on underlying connection
+func NewBufferedConn(conn net.Conn, readBufferSize, writeBufferSize int) net.Conn {
+	bc := &bufferedConn{
+		Conn:      conn,
+		bufReader: bufio.NewReaderSize(conn, readBufferSize),
+		bufWriter: bufio.NewWriterSize(conn, writeBufferSize),
+	}
+	return bc
+}
+
+// bufferedConn provides buffered write to connections
+type bufferedConn struct {
+	net.Conn
+	bufReader *bufio.Reader
+	bufWriter *bufio.Writer
+}
+
+// Read
+func (bc *bufferedConn) Read(p []byte) (int, error) {
+	return bc.bufReader.Read(p)
+}
+
+func (bc *bufferedConn) Write(p []byte) (int, error) {
+	return bc.bufWriter.Write(p)
+}
+
+func (bc *bufferedConn) Close() error {
+	bc.Flush()
+	return bc.Conn.Close()
+}
+
+func (bc *bufferedConn) Flush() error {
+	err := bc.bufWriter.Flush()
+	if err != nil {
+		return err
+	}
+
+	if f, ok := bc.Conn.(Flushable); ok {
+		return f.Flush()
+	}
+
+	return nil
+}
---
+++ types.go
@@ -0,0 +1,5 @@
+package netconnutil
+
+type Flushable interface {
+	Flush() error
+}
